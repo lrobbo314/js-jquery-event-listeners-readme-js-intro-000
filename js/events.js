9e8e5c6a6f142d27ed72d3eb0cf52bd047f3961a
Robert Lego <robertlego@gmail.com>
2018-03-03 00:54:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,9 +1,33 @@
 //define functions here
 
-$('p').on("keypress", getIt() {
-  alert("Hey!");
-  return;
-});
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+    let i = document.getElementsByTagName('img')[0];
+    i.style.border = '2px solid #E8272C';
+    console.log(i);
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert(key.value + ' was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  });
+}
 
 $(document).ready(function() {
   // call functions here
