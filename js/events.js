46ff72d3c0d9e40ecfb4e87e01c700418fed6a67
Robert Lego <robertlego@gmail.com>
2018-03-03 00:22:24 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,7 +6,7 @@ function getIt() {
   });
 }
 
-  $(document).ready(function() {
-    // call functions here
-    getIt();
-  });
+$(document).ready(function() {
+  // call functions here
+  getIt();
+});
