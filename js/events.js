5d9478023aad685ad41a8d5b7c1389ba6b811a79
Robert Lego <robertlego@gmail.com>
2018-03-03 00:18:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,8 +1,14 @@
 //define functions here
 
-$('p').on("keypress", getIt() {
-  alert("Hey!");
-  return;
+// $('p').on("keypress", getIt() {
+//   alert("Hey!");
+//   return;
+// });
+
+$(document).on('keypress', function(key) {
+  if (key.which == 13){
+    alert('enter was pressed');
+  }
 });
 
 $(document).ready(function() {
