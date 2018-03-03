c9ac99c6969241ae950895af83fbdd3dfd8a5485
Robert Lego <robertlego@gmail.com>
2018-03-03 00:19:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,9 +1,9 @@
 //define functions here
 
-// $('p').on("keypress", getIt() {
-//   alert("Hey!");
-//   return;
-// });
+$('p').on("keypress", getIt() {
+  alert("Hey!");
+  return;
+});
 
 $(document).on('keypress', function(key) {
   if (key.which == 13) {
